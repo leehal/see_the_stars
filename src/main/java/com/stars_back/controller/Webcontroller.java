16455f474958297aef821777466e0b88b9b93f34
YUY <yuy8150@gmail.com>
2024-07-18 17:53:50 +0900

    07.18

--- src/main/java/com/stars_back/controller/Webcontroller.java
+++ src/main/java/com/stars_back/controller/Webcontroller.java
@@ -1,9 +1,11 @@
 package com.stars_back.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class Webcontroller implements ErrorController {
     private static final String PATH = "/error";
 
